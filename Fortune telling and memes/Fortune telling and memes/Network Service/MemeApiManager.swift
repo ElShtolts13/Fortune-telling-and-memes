9a23c1754f03bd7f2fe07430ae5567a74b08e3b3
Nadezhda <el.shtolts@yandex.ru>
2025-02-04 21:38:05 +0300

    added network service

--- Fortune telling and memes/Fortune telling and memes/Network Service/MemeApiManager.swift
+++ Fortune telling and memes/Fortune telling and memes/Network Service/MemeApiManager.swift
@@ -6,39 +6,69 @@
 //
 
 import Foundation
+import UIKit
 
-final class MemeAPIManager {
+final class APIManager {
     
-
+    static let shared = APIManager()
+    private let casheImage = NSCache<NSString, UIImage>()
+    private init() {}
     
-
-  
-    func fetchData(oneComplection: @escaping([Meme]) -> ()) {
-
+    func fetchMemes(completion: @escaping (Result<[Meme], Error>) -> Void) {
         let urlString = "https://api.imgflip.com/get_memes"
-
+        
         guard let url = URL(string: urlString) else {
-            print("Неправильный URL")
+            completion(.failure(NetworkError.invalidURL))
             return
         }
-
-      let task = URLSession.shared.dataTask(with: url) {(data, resp, error) in
-            
-          if let error = error {
-              print("Error: \(error.localizedDescription)")
-              return
-          }
-            guard  let data = data else {
-                print("data was nil")
+        
+        URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
                 return
             }
             
-            guard let  MemeData = try? JSONDecoder().decode(MemeData.self, from: data) else {
-                print("couldn't decode json")
+            guard let data = data else {
+                completion(.failure(NetworkError.noData))
                 return
             }
-          //oneComplection(MemeData.memes)
-        }
-        task.resume()
+            
+            do {
+                let decoder = JSONDecoder()
+                let response = try decoder.decode(MemeResponse.self, from: data)
+                completion(.success(response.data.memes))
+            } catch {
+                completion(.failure(error))
+            }
+        }.resume()
     }
+    
+    func loadImage(from urlString: String, completion: @escaping (UIImage?) -> Void) {
+        if let cachedImage = imageCache.object(forKey: urlString as NSString) {
+            completion(cachedImage)
+            return
+        }
+        
+        guard let url = URL(string: urlString) else {
+            completion(nil)
+            return
+        }
+        
+        URLSession.shared.dataTask(with: url) { [weak self] data, _, _ in
+            guard let data = data, let image = UIImage(data: data) else {
+                completion(nil)
+                return
+            }
+            
+            self?.imageCache.setObject(image, forKey: urlString as NSString)
+            completion(image)
+        }.resume()
+    }
+
+enum NetworkError: Error {
+    case invalidURL
+    case noData
+    case decodingError
+}
+
 }
